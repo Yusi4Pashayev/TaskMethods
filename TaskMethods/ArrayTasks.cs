e98eed9ba8ea00e1d206dd3834837cd6ce7614e2
Jarvis <Jarvis@DESKTOP-ESJP0TC>
2023-01-12 04:27:01 +0400

    Add project files.

---
+++ TaskMethods/ArrayTasks.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMethods
+{
+    internal class ArrayTasks
+    {
+        internal int[] EnterAray()
+        {
+            Console.Write("Masssivin uzunlugunu daxil edin: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] mass = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"mass{i}=");
+                mass[i] = int.Parse(Console.ReadLine());
+            }
+
+            return mass;
+        }
+
+        internal int[] FindNumbers(int[] numbers)
+        {
+            int newarraylength = 0, index = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double b = Math.Sqrt(numbers[i]);
+                if (b % 1 == 0)
+                {
+                    newarraylength++;
+                }
+
+            }
+
+            int[] newarray = new int[newarraylength];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double b = Math.Sqrt(numbers[i]);
+                if (b % 1 == 0)
+                {
+                    newarray[index] = numbers[i];
+                    index++;
+                }
+
+            }
+
+
+            return newarray;
+        }
+
+        internal void Print(int[] massiv)
+        {
+            foreach (var item in massiv)
+            {
+                Console.Write($"{item,5}");
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+
+    internal class NumberTasks
+    {
+        internal int EnterNumber()
+        {
+            Console.Write("Eded daxil edin:");
+            int newnumber = int.Parse(Console.ReadLine());
+            return newnumber;
+        }
+
+        internal int ReserveNumber(int number)
+        {
+            int Reservenumber = 0, a;
+            for (int i = (int)Math.Log10(number); i >= 0; i--)
+            {
+                a = number % 10;
+                number = number / 10;
+                Reservenumber += a * (int)Math.Pow(10, i);
+            }
+
+            return Reservenumber;
+        }
+
+        internal void Print(int Number)
+        {
+            Console.WriteLine(Number);
+        }
+
+    }
+}
---
+++ TaskMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TaskMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TASK1
+            //var arrayTasks = new ArrayTasks();
+
+            //var mass = arrayTasks.EnterAray();
+
+            //arrayTasks.Print(mass);
+
+            //var newarray = arrayTasks.FindNumbers(mass);
+            //Console.WriteLine("verilen massivde kvadrat koku olan ededlerden ibaret yeni massviv");
+            //arrayTasks.Print(newarray);
+
+            #endregion
+
+            #region TASK2
+
+            var numberTasks = new NumberTasks();
+
+            var newnumber = numberTasks.EnterNumber();
+            
+            Console.Write("Number is ");
+
+            numberTasks.Print(newnumber);
+
+            var returnnumber = numberTasks.ReserveNumber(newnumber);
+
+            Console.Write("Reserve number is ");
+            numberTasks.Print(returnnumber);
+
+            #endregion
+        }
+    }
+}
